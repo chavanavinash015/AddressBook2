dab12c8b050411579d5d76bd9ea1d80a4e15b3d4
arc-015 <chavanavinash015@gmail.com>
2022-11-17 13:08:23 +0530

    Added : add  Contact address Book

--- src/com/bridelabz/AddressBookMain.java
+++ src/com/bridelabz/AddressBookMain.java
@@ -1,11 +1,50 @@
 package com.bridelabz;
 
-public class AddressBookMain {
+import java.util.Scanner;
 
+public class AddressBookMain {
+    public static Scanner scanner = new Scanner(System.in);
+    public static void addContactDetails() {
+        Contact contact = new Contact();
+        System.out.println("Enter First name");
+        String firstName = scanner.next();
+        contact.setFirstName(firstName);
+
+        System.out.println("Enter Last name");
+        String lastName = scanner.next();
+        contact.setLastName(lastName);
+
+        System.out.println("Enter Address");
+        String address = scanner.next();
+        contact.setAddress(address);
+
+        System.out.println("Enter City name");
+        String city = scanner.next();
+        contact.setCity(city);
+
+        System.out.println("Enter State name");
+        String state = scanner.next();
+        contact.setState(state);
+
+        System.out.println("Enter Zip code");
+        String zip = scanner.next();
+        contact.setZip(zip);
+
+        System.out.println("Enter Phone number");
+        String phoneNumber = scanner.next();
+        contact.setPhoneNumber(phoneNumber);
+
+        System.out.println("Enter Email");
+        String email = scanner.next();
+        contact.setEmail(email);
+
+        System.out.println(contact);
+
+    }
     public static void main(String[] args) {
 
         System.out.println("Welcome to the AddressBook Program ");
-   
+
 
     }
 }
